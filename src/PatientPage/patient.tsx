0acd35b810dbf57bbe67b602efd67ce184c18f97
Melimet <erkka.rahikainen@gmail.com>
2022-10-14 21:32:12 +0300

    9.17

---
+++ src/PatientPage/patient.tsx
@@ -0,0 +1,27 @@
+import { useParams } from 'react-router-dom';
+import { useStateValue } from '../state';
+import { Patient } from '../types';
+import { State } from '../state';
+
+function getPatient(state: State, id: string ):Patient | undefined{
+  
+  return state.patients[id];    
+}
+
+function PatientPage() {
+  const [state, ] = useStateValue();
+  const id = useParams().id;
+  if (!id) return <div>nada id</div>;
+
+  const patient = getPatient(state, id);
+
+  if (!patient) return <div>nada patiento</div>;
+
+  return <article>
+      <h2>{patient.name}, {patient.gender}</h2>
+      <p>DoB: {patient.dateOfBirth}</p>
+      <p>occupation: {patient.occupation}</p>
+    </article>;
+}
+
+export { PatientPage };

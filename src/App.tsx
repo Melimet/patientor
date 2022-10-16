4edfb2b370699641e1b61a3cbcfc77a475d4a61f
Melimet <erkka.rahikainen@gmail.com>
2022-10-16 20:11:48 +0300

    9.21

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { Button, Divider, Container } from "@material-ui/core";
 
 import { apiBaseUrl } from "./constants";
-import { useStateValue, setPatients } from "./state";
-import { Patient } from "./types";
+import { useStateValue, setPatients, setDiagnosis } from "./state";
+import { Diagnosis, Patient } from "./types";
 
 import PatientListPage from "./PatientListPage";
 import { Typography } from "@material-ui/core";
@@ -26,8 +26,22 @@ const App = () => {
         console.error(e);
       }
     };
+
+    async function fetchDiagnosisList(){
+      try{
+        const { data: diagnosisListFromApi } = await axios.get<Diagnosis[]>(
+          `${apiBaseUrl}/diagnoses`
+        );
+        dispatch(setDiagnosis(diagnosisListFromApi));
+      } catch(e) {
+        console.error(e);
+      }
+    }
     void fetchPatientList();
+    void fetchDiagnosisList();
   }, [dispatch]);
+
+  
 
   return (
     <div className="App">
--- src/PatientPage/patient.tsx
+++ src/PatientPage/patient.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useStateValue } from '../state';
-import { Patient } from '../types';
+import { Diagnosis, Patient } from '../types';
 import { State } from '../state';
 
 function getPatient(state: State, id: string ):Patient | undefined{
@@ -8,9 +8,14 @@ function getPatient(state: State, id: string ):Patient | undefined{
   return state.patients[id];    
 }
 
+function getDiagnosis(state: State, code:string):Diagnosis | undefined{
+  return state.diagnoses[code];
+}
+
 function PatientPage() {
   const [state, ] = useStateValue();
   const id = useParams().id;
+
   if (!id) return <div>nada id</div>;
 
   const patient = getPatient(state, id);
@@ -25,7 +30,10 @@ function PatientPage() {
         {patient.entries.map((entry)=> 
         <div style={{borderStyle: "solid", padding:"1rem", margin:"1rem"}}key={entry.id}>
           {entry.date}, <i>{entry.description}</i> 
-          <p>{entry.diagnosisCodes ? entry.diagnosisCodes.join(", "): ""}</p>
+          <ul>
+            {entry.diagnosisCodes?.map((diagnose) => 
+            <li key={diagnose}>{diagnose} - {getDiagnosis(state, diagnose)?.name}</li>)}
+          </ul>
         <p>Diagnosis by {entry.specialist}</p>
         </div>
         )}
